2014981900653af6566c520a4c524d58f86a8490
Joseph Santiago <santiago.jospeh92@csu.fullerton.edu>
2020-04-28 16:14:16 -0700

    working on Protection

--- src/HowToProtectYourself.js
+++ src/HowToProtectYourself.js
@@ -1,7 +1,55 @@
 import React from 'react'
 
 export const HowToProtectYourself = () => (
-    <div>
-        <h1>Protect</h1>
+ <div>
+        <h3><i class="fas fa-viruses"></i>  Stopping the spread starts with knowing how it spreads</h3>
+        <ul>
+            <li>The virus spreads from person to person</li>
+            <li>Between people who are in close contact with one another (within about 6 feet).</li>
+            <li>Through respiratory droplets produced when an infected person coughs, sneezes or talks.</li>
+            <li>Through respiratory droplets produced when an infected person coughs, sneezes or talks.</li>
+            <li>These droplets can land in the mouths or noses of people who are nearby or possibly be inhaled into the lungs.</li>
+            <li>Some recent studies have suggested that COVID-19 may be spread by people who are not showing symptoms.</li>
+        </ul>
+
+        <h3><i class="fas fa-hand-sparkles"></i>  Washing your hands often will helps stop the spread</h3>
+        <ul>
+            <li>Wash your hands often with soap and water for at least 20 seconds especially after you have been in a public place, or after blowing your nose, coughing, or sneezing.</li>
+            <li>If soap and water are not readily available, use a hand sanitizer that contains at least 60% alcohol. Cover all surfaces of your hands and rub them together until they feel dry.</li>
+            <li>Avoid touching your eyes, nose, and mouth with unwashed hands.</li>
+        </ul>
+
+        <h3><i class="fas fa-male"></i><i class="fas fa-arrows-alt-h"></i><i class="fas fa-female"></i>  Avoid close contact</h3>
+        <ul>
+            <li>Avoid close contact with people who are sick</li>
+            <li>Stay home as much as possible.</li>
+            <li>Put distance between yourself and other people.</li>
+            <li>Remember that some people without symptoms may be able to spread virus.</li>
+            <li>Keeping distance from others is especially important for people who are at higher risk of getting very sick.</li>
+        </ul>
+
+        <h3><i class="fas fa-head-side-mask"></i>  Cover your mouth and nose with a cloth face cover when around others</h3>
+        <ul>
+            <li>You could spread COVID-19 to others even if you do not feel sick.</li>
+            <li>Everyone should wear a cloth face cover when they have to go out in public, for example to the grocery store or to pick up other necessities.
+Cloth face coverings should not be placed on young children under age 2, anyone who has trouble breathing, or is unconscious, incapacitated or otherwise unable to remove the mask without assistance.</li>
+            <li>The cloth face cover is meant to protect other people in case you are infected.</li>
+            <li>Do NOT use a facemask meant for a healthcare worker.</li>
+            <li>Continue to keep about 6 feet between yourself and others. The cloth face cover is not a substitute for social distancing.</li>
+        </ul>
+
+        <h3><i class="fas fa-head-side-cough-slash"></i> <i class="fas fa-box-tissue"></i>  Cover coughs and sneezes</h3>
+        <ul>
+            <li>If you are in a private setting and do not have on your cloth face covering, remember to always cover your mouth and nose with a tissue when you cough or sneeze or use the inside of your elbow.</li>
+            <li>Throw used tissues in the trash.</li>
+            <li>Immediately wash your hands with soap and water for at least 20 seconds. If soap and water are not readily available, clean your hands with a hand sanitizer that contains at least 60% alcohol.</li>
+        </ul>
+
+        <h3><i class="fas fa-pump-medical"></i>  Clean and disinfect</h3>
+        <ul>
+            <li>Clean AND disinfect frequently touched surfaces daily. This includes tables, doorknobs, light switches, countertops, handles, desks, phones, keyboards, toilets, faucets, and sinks.</li>
+            <li>If surfaces are dirty, clean them. Use detergent or soap and water prior to disinfection.</li>
+            <li>Then, use a household disinfectant. Most common EPA-registered household disinfectants will work.</li>
+        </ul>
     </div>
 )
